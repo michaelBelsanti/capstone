7337aa91820c215ecc3edf84379a2eec7906e2de
michaelBelsanti <quasigod-io@protonmail.com>
2024-02-08 15:30:00 -0500

    add nix code to build package

--- flake.nix
+++ flake.nix
@@ -7,5 +7,17 @@
     flakelight ./. {
       inherit inputs;
       devShell.packages = pkgs: with pkgs; [ nodejs ];
+      package = { buildNpmPackage }:
+        buildNpmPackage {
+          name = "capstone";
+          version = "1.0.0";
+          src = ./.;
+          npmDepsHash = "sha256-YU61KGCOT6wapsmPhZsmaptHX5vaYQyEobBq2ZmCIEw=";
+          installPhase = ''
+            mkdir $out
+            npm run build
+            cp -r dist/* $out
+          '';
+        };
     };
 }
