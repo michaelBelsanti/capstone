600615c31934f4ac53b48fcb458aae0846c89160
VF9876 <159855655+VF9876@users.noreply.github.com>
2024-02-21 11:18:58 -0500

    Updated map_utils.js to add marker

--- map_utils.js
+++ map_utils.js
@@ -1,38 +1,45 @@
 export function initMap() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: initialLocation,
-                zoom: 14
-            });
-            // Customize the map as needed
-            var marker = new google.maps.Marker({
-                position: initialLocation,
-                map: map,
-                title: 'You are here!'
-            });
-        }, function() {
-            handleLocationError(true, map ? map.getCenter() : null);
-        });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, null);
-    }
-
-    return map;
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+          var initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+          map = new google.maps.Map(document.getElementById('map'), {
+              center: initialLocation,
+              zoom: 14
+          });
+          // Customize the map as needed
+          var marker = new google.maps.Marker({
+              position: initialLocation,
+              map: map,
+              icon:'https://img.icons8.com/fluent/48/000000/marker-storm.png',
+              draggarble: true
+          });
+      }, function() {
+          handleLocationError(true, map ? map.getCenter() : null);
+      });
+  } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, null);
+  }
+  var information = new google.maps.InfoWindow({
+    content: '<h4>New Marker</h4>'
+ });
+  marker.addListener('click', function() {
+    information.open(map, marker);
+  });
+  return map;
 }
 
 export function handleLocationError(browserHasGeolocation, initialLocation) {
-  var error = browserHasGeolocation ?
-                'Error: The Geolocation service failed.' :
-                'Error: Your browser doesn\'t support geolocation.';
-  alert(error);
-  if (initialLocation) {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: initialLocation,
-      zoom: 14
-    });
-  }
+var error = browserHasGeolocation ?
+              'Error: The Geolocation service failed.' :
+              'Error: Your browser doesn\'t support geolocation.';
+alert(error);
+if (initialLocation) {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: initialLocation,
+    zoom: 14
+  });
 }
+}
+
 
