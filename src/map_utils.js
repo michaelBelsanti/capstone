cf0b3a8f9dabea5c7a394c87381d789438be1672
michaelBelsanti <quasigod-io@protonmail.com>
2024-02-29 11:35:25 -0500

    formatted src/map_utils.js

--- src/map_utils.js
+++ src/map_utils.js
@@ -1,28 +1,28 @@
 export function initMap() {
   if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-          var initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-          map = new google.maps.Map(document.getElementById('map'), {
-              center: initialLocation,
-              zoom: 14
-          });
-          // Customize the map as needed
-          var marker = new google.maps.Marker({
-              position: initialLocation,
-              map: map,
-              icon:'https://img.icons8.com/fluent/48/000000/marker-storm.png',
-              draggarble: true
-          });
-      }, function() {
-          handleLocationError(true, map ? map.getCenter() : null);
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: initialLocation,
+        zoom: 14
       });
+      // Customize the map as needed
+      var marker = new google.maps.Marker({
+        position: initialLocation,
+        map: map,
+        icon: 'https://img.icons8.com/fluent/48/000000/marker-storm.png',
+        draggarble: true
+      });
+    }, function() {
+      handleLocationError(true, map ? map.getCenter() : null);
+    });
   } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, null);
+    // Browser doesn't support Geolocation
+    handleLocationError(false, null);
   }
   var information = new google.maps.InfoWindow({
     content: '<h4>New Marker</h4>'
- });
+  });
   marker.addListener('click', function() {
     information.open(map, marker);
   });
@@ -30,16 +30,16 @@ export function initMap() {
 }
 
 export function handleLocationError(browserHasGeolocation, initialLocation) {
-var error = browserHasGeolocation ?
-              'Error: The Geolocation service failed.' :
-              'Error: Your browser doesn\'t support geolocation.';
-alert(error);
-if (initialLocation) {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: initialLocation,
-    zoom: 14
-  });
-}
+  var error = browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.';
+  alert(error);
+  if (initialLocation) {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: initialLocation,
+      zoom: 14
+    });
+  }
 }
 
 
