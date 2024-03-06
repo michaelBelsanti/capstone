a48d48d9953f69d60a58648b0c789237499bfc9e
VF9876 <159855655+VF9876@users.noreply.github.com>
2024-03-06 17:01:15 -0500

    Added search bar + new markers

--- src/map_utils.js
+++ src/map_utils.js
@@ -4,15 +4,72 @@ export function initMap() {
       var initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
       map = new google.maps.Map(document.getElementById('map'), {
         center: initialLocation,
-        zoom: 14
+        zoom: 14,
+        mapTypeId: "roadmap",
       });
-      // Customize the map as needed
-      var marker = new google.maps.Marker({
-        position: initialLocation,
-        map: map,
-        icon: 'https://img.icons8.com/fluent/48/000000/marker-storm.png',
-        draggarble: true
-      });
+
+      const input = document.getElementById("pac-input");
+      const searchBox = new google.maps.places.SearchBox(input);
+
+      map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // Bias the SearchBox results towards current map's viewport.
+      map.addListener("bounds_changed", () => {
+      searchBox.setBounds(map.getBounds());
+  });
+
+  let markers = [];
+
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener("places_changed", () => {
+    const places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach((marker) => {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    const bounds = new google.maps.LatLngBounds();
+
+    places.forEach((place) => {
+      if (!place.geometry || !place.geometry.location) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+
+      const icon = {
+        url: 'https://img.icons8.com/fluent/48/000000/marker-storm.png',
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25),
+      };
+
+      // Create a marker for each place.
+      markers.push(
+        new google.maps.Marker({
+          map,
+          icon,
+          title: place.name,
+          position: place.geometry.location,
+        }),
+      );
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  }); // Customize the map as needed
+   
     }, function() {
       handleLocationError(true, map ? map.getCenter() : null);
     });
@@ -23,9 +80,7 @@ export function initMap() {
   var information = new google.maps.InfoWindow({
     content: '<h4>New Marker</h4>'
   });
-  marker.addListener('click', function() {
-    information.open(map, marker);
-  });
+  
   return map;
 }
 
@@ -41,5 +96,8 @@ export function handleLocationError(browserHasGeolocation, initialLocation) {
     });
   }
 }
+
+
+
 
 
